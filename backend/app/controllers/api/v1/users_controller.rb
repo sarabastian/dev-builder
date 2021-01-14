cc245f593473911c333ce5e3b35a0adb09000849
sarabastian <seb118@georgetown.edu>
2021-01-14 17:08:21 -0500

    jwt

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-
+    skip_before_action :authorized, only: [:create]
     def index
         users = User.all
         render json: users 
@@ -10,14 +10,20 @@ class Api::V1::UsersController < ApplicationController
         render json: user
     end
 
-    def create 
-        user = User.new(user_params)
-        if @user.valid?
-            render json: { user: UserSerializer.new(@user) }, status: :created
-          else
-            render json: { error: 'failed to create user' }, status: :not_acceptable
-          end
+    def profile
+        render json: { user: UserSerializer.new(current_user) }, status: :accepted
     end
+    
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+          @token = encode_token(user_id: @user.id)
+          render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+        else
+          render json: { error: 'failed to create user' }, status: :not_acceptable
+        end
+      end
+    
 
     def update
         user = User.find(params[:id])
